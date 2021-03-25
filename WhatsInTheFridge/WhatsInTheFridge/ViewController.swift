8f293aa8e0ddcb5dd8c982d7d18d8c80db931e2a
Suqian Wang <suqianwang@Suqians-MacBook-Pro.local>
2021-03-25 16:44:45 -0400

    added partial storyboard structure

---
+++ WhatsInTheFridge/WhatsInTheFridge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatsInTheFridge
+//
+//  Created by Suqian Wang on 3/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
