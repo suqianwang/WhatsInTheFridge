fdfa78b95e1e4c5bda48fb9438ca72a226c0aa9f
Billy Luqiu <wyl6@duke.edu>
2021-04-04 02:13:54 -0400

    added separate API calls to get recipe detail for each recipe loaded

--- WhatsInTheFridge/WhatsInTheFridge/RecipeTableViewController.swift
+++ WhatsInTheFridge/WhatsInTheFridge/RecipeTableViewController.swift
@@ -32,13 +32,150 @@ class RecipeTableViewController: UITableViewController {
         let extendedName: String?
     }
     
-    var recipes:[recipe] = []
+
+    // MARK: - Welcome
+    struct recipeDetail: Codable {
+        let vegetarian, vegan, glutenFree, dairyFree: Bool
+        let veryHealthy, cheap, veryPopular, sustainable: Bool
+        let weightWatcherSmartPoints: Int
+        let gaps: String
+        let lowFodmap: Bool
+        let preparationMinutes, cookingMinutes, aggregateLikes, spoonacularScore: Int
+        let healthScore: Int
+        let creditsText, sourceName: String
+        let pricePerServing: Double
+        let extendedIngredients: [ExtendedIngredient]
+        let id: Int
+        let title: String
+        let readyInMinutes, servings: Int
+        let sourceURL: String
+        let image: String
+        let imageType, summary: String
+        let cuisines, dishTypes, diets, occasions: [String]
+        let winePairing: WinePairing
+        let instructions: String
+        let analyzedInstructions: [AnalyzedInstruction]
+        let originalID: JSONNull?
+
+        enum CodingKeys: String, CodingKey {
+            case vegetarian, vegan, glutenFree, dairyFree, veryHealthy, cheap, veryPopular, sustainable, weightWatcherSmartPoints, gaps, lowFodmap, preparationMinutes, cookingMinutes, aggregateLikes, spoonacularScore, healthScore, creditsText, sourceName, pricePerServing, extendedIngredients, id, title, readyInMinutes, servings
+            case sourceURL = "sourceUrl"
+            case image, imageType, summary, cuisines, dishTypes, diets, occasions, winePairing, instructions, analyzedInstructions
+            case originalID = "originalId"
+        }
+    }
+
+    // MARK: - AnalyzedInstruction
+    struct AnalyzedInstruction: Codable {
+        let name: String
+        let steps: [Step]
+    }
+
+    // MARK: - Step
+    struct Step: Codable {
+        let number: Int
+        let step: String
+        let ingredients, equipment: [Ent]
+        let length: Length?
+    }
+
+    // MARK: - Ent
+    struct Ent: Codable {
+        let id: Int
+        let name, localizedName, image: String
+    }
+
+    // MARK: - Length
+    struct Length: Codable {
+        let number: Int
+        let unit: String
+    }
+
+    // MARK: - ExtendedIngredient
+    struct ExtendedIngredient: Codable {
+        let id: Int
+        let aisle, image, consistency, name: String
+        let nameClean, original, originalString, originalName: String
+        let amount: Double
+        let unit: String
+        let meta, metaInformation: [String]
+        let measures: Measures
+    }
+
+    // MARK: - Measures
+    struct Measures: Codable {
+        let us, metric: Metric
+    }
+
+    // MARK: - Metric
+    struct Metric: Codable {
+        let amount: Double
+        let unitShort, unitLong: String
+    }
+
+    // MARK: - WinePairing
+    struct WinePairing: Codable {
+        let pairedWines: [String]
+        let pairingText: String
+        let productMatches: [ProductMatch]
+    }
+
+    // MARK: - ProductMatch
+    struct ProductMatch: Codable {
+        let id: Int
+        let title, productMatchDescription, price: String
+        let imageURL: String
+        let averageRating: Double
+        let ratingCount: Int
+        let score: Double
+        let link: String
+
+        enum CodingKeys: String, CodingKey {
+            case id, title
+            case productMatchDescription = "description"
+            case price
+            case imageURL = "imageUrl"
+            case averageRating, ratingCount, score, link
+        }
+    }
+
+    // MARK: - Encode/decode helpers
+
+    class JSONNull: Codable, Hashable {
+
+        public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+            return true
+        }
+
+        public var hashValue: Int {
+            return 0
+        }
+
+        public init() {}
+
+        public required init(from decoder: Decoder) throws {
+            let container = try decoder.singleValueContainer()
+            if !container.decodeNil() {
+                throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+            }
+        }
+
+        public func encode(to encoder: Encoder) throws {
+            var container = encoder.singleValueContainer()
+            try container.encodeNil()
+        }
+    }
+
     
+    
+    
+    var recipes:[recipe] = []
+    var recipeDetailList:[recipeDetail] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.rowHeight = 150
-        let ingredients: [String] = ["Apples", "Flour", "Sugar"]
+        let ingredients: [String] = ["Ribeye"]
         getAllData(ingredients: ingredients)
 
         // Uncomment the following line to preserve selection between presentations
@@ -72,7 +209,7 @@ class RecipeTableViewController: UITableViewController {
         let urlRequestString: String = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients="+String(ingredientsString)+"&number=5&ranking=1&ignorePantry=true"
         
         let urlStr = URL(string: urlRequestString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
-        
+        print(urlStr)
         let request = NSMutableURLRequest(url: urlStr!,
                                                 cachePolicy: .useProtocolCachePolicy,
                                             timeoutInterval: 10.0)
@@ -98,6 +235,7 @@ class RecipeTableViewController: UITableViewController {
             
             do{
                 self.recipes = try JSONDecoder().decode([recipe].self, from: jsonData)
+                self.fillRecipeDetailData()
                 //because we HAVE to refresh after we load the data to make sure the data is populated.
                 //this is a separate task so we gotta use dispatch queue to tell it to go to the main thread
                 DispatchQueue.main.async {
@@ -110,6 +248,52 @@ class RecipeTableViewController: UITableViewController {
         
         dataTask.resume()
 
+    }
+    
+    func fillRecipeDetailData() {
+        for recipe in recipes   {
+            let id = recipe.id
+            let headers = [
+                "x-rapidapi-key": "6f1810ca34msh227332a299bf704p13f30bjsn1ba98259af85",
+                "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+            ]
+            let urlRequestString: String = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"+String(id)+"/information"
+            
+            let urlStr = URL(string: urlRequestString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!)
+            
+            let request = NSMutableURLRequest(url: urlStr!,
+                                                    cachePolicy: .useProtocolCachePolicy,
+                                                timeoutInterval: 10.0)
+            request.httpMethod = "GET"
+            request.allHTTPHeaderFields = headers
+
+            let session = URLSession.shared
+            let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+                guard error == nil else {
+                    print ("Error: \(error!)")
+                    DispatchQueue.main.async {
+                        let alert = UIAlertController(title: "Error - ", message: "\(error!)", preferredStyle: .alert)
+                        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                        self.present(alert, animated: true)
+                    }
+                    return
+                }
+                
+                guard let jsonData = data else {
+                    print("No data")
+                    return
+                }
+                
+                do{
+                    self.recipeDetailList.append(try JSONDecoder().decode(recipeDetail.self, from: jsonData))
+                    print("here")
+                } catch {
+                    print("JSONDecoder error: \(error)")
+                }
+            })
+
+            dataTask.resume()
+        }
     }
     
     
