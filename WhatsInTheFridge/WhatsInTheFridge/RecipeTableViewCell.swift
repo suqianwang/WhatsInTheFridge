4526e61a9721af7087411241589f25b686a1cc12
Billy Luqiu <wyl6@duke.edu>
2021-04-03 22:52:23 -0400

    adding templates for controller files

---
+++ WhatsInTheFridge/WhatsInTheFridge/RecipeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  RecipeTableViewCell.swift
+//  WhatsInTheFridge
+//
+//  Created by Billy Luqiu on 4/3/21.
+//
+
+import UIKit
+
+class RecipeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
