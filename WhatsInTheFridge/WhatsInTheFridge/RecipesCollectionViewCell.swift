26b93ebbbcc72f9a6667aa9441462e30934f996d
Suqian Wang <suqianwang1996@gmail.com>
2021-04-05 01:20:32 -0400

    added addition to gitignore to ignore breakpoints

---
+++ WhatsInTheFridge/WhatsInTheFridge/RecipesCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  RecipesCollectionViewCell.swift
+//  WhatsInTheFridge
+//
+//  Created by Suqian Wang on 4/4/21.
+//
+
+import UIKit
+
+class RecipesCollectionViewCell: UICollectionViewCell {
+    
+}
