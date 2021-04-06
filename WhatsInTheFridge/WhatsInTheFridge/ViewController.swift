a44ec9c190e34f616ab402f1abc0c849963757d6
Suqian Wang <suqianwang@Suqians-MacBook-Pro.local>
2021-04-06 14:48:05 -0400

    project milestone

---
+++ WhatsInTheFridge/WhatsInTheFridge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsInTheFridge
+//
+//  Created by Suqian Wang on 3/20/21.
+//
+
+import UIKit
+import ResearchKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
